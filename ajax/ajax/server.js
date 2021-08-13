70e1b0de08a89d30401bedd275aa2990c33a8c92
eduardo7321 <eduardo.jose184@gmail.com>
2021-08-12 22:10:06 -0300

    AJAX - aula 2 de fetch API

--- ajax/ajax/server.js
+++ ajax/ajax/server.js
@@ -1,23 +1,10 @@
-/*const bodyParser = require('body-parser')
+const bodyParse = require('body-parser')
 const express = require('express')
 const app = express()
 
 app.use(express.static('.'))
-app.use(express.urlencoded({ extended: true}))
-app.use(express.json())
-
-app.get('/teste', (req, res) => res.send('OK'))
-app.listen(8080, () => console.log('Executando...start'))*/
-
-
-
-const bodyParser = require('body-parser')
-const express = require('express')
-const app = express()
-
-app.use(express.static('.'))
-app.use(bodyParser.urlencoded({ extended: true}))
-app.use(bodyParser.json())
+app.use(bodyParse.urlencoded({ extended: true}))
+app.use(bodyParse.json())
 
 const multer = require('multer')
 
@@ -42,5 +29,46 @@ app.post('/upload', (req, res) => {
     })
 })
 
+app.post('/formulario', (req, res) => {
+    res.send({
+        ...req.body,
+        id: 7
+    })
+})
+
 app.get('/teste', (req, res) => res.send(new Date))
 app.listen(8080, () => console.log('Executando...start'))
+
+
+
+/*const bodyParser = require('body-parser')
+const express = require('express')
+const app = express()
+
+app.use(express.static('.'))
+app.use(express.urlencoded({ extended: true}))
+app.use(express.json())
+
+app.get('/teste', (req, res) => res.send('OK'))
+app.listen(8080, () => console.log('Executando...start'))*/
+
+
+
+//app.use(express.urlencoded({ extended: true}))
+//app.use(express.json())
+/*
+//teste
+//const express = require('express');
+const consign = require('consign');
+
+module.exports = () => {    
+    const app = express();
+    app.use(express.json())
+    app.use(express.urlencoded({ extended: true}))
+    consign()
+        .include('controllers')
+        .into(app)
+    return app;
+}
+//teste
+*/
