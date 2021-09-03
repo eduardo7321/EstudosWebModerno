301d9bf253631ad1d1c1a2c88b12d23f25910885
eduardo7321 <eduardo.jose184@gmail.com>
2021-09-02 21:42:10 -0300

    add file Multiplos.jsx

--- react/exercicios/src/index.js
+++ react/exercicios/src/index.js
@@ -1,8 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-import Primeiro from './componentes/Primeiro'
+//import Primeiro from './componentes/Primeiro'
+//import BomDia from './componentes/BomDia'
 
-const elemento = <h1>React 2</h1>
+import { BoaTarde, BoaNoite} from './componentes/Multiplos'
 
-ReactDOM.render(<Primeiro/>, document.getElementById('root'))
+ReactDOM.render(
+    <div>
+        <BoaTarde nome="Eduardo" />
+        <BoaNoite nome="Dudu" />
+    </div>
+    , document.getElementById('root'))
