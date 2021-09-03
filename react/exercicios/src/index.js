ba3ae845142d1529aba8a2eb4f85e046d867f26e
Eduardo.silva <eduardo.jose184@gmail.com>
2021-09-03 16:59:52 -0300

    react - componentes pai e filho 01

--- react/exercicios/src/index.js
+++ react/exercicios/src/index.js
@@ -10,11 +10,17 @@ import ReactDOM from 'react-dom'
 //import { BoaTarde, BoaNoite} from './componentes/Multiplos'
 
 
-import Saudacao from './componentes/Saudacao'
+//import Saudacao from './componentes/Saudacao'
+import Pai from './componentes/Pai'
+import Filho from './componentes/Filho'
 
 ReactDOM.render(
     <div>        
-        <Saudacao tipo="Bom dia" nome="Dudu" />
+        <Pai nome="eudedu" sobrenome="silva" >
+            <Filho nome="Filho1" sobrenome="Silva" />
+            <Filho nome="Filho2" sobrenome="Silva" />
+            <Filho nome="Filho3" sobrenome="Silva" />
+        </Pai>
     </div>
     , document.getElementById('root'))
 
