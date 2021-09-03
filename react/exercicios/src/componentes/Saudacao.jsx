a6783fad6dcb5cacd38e47ad025403c97e0b2f34
Eduardo.silva <eduardo.jose184@gmail.com>
2021-09-03 15:04:48 -0300

    react - aula 02 componente de classe

--- react/exercicios/src/componentes/Saudacao.jsx
+++ react/exercicios/src/componentes/Saudacao.jsx
@@ -3,14 +3,20 @@ import React, { Component } from 'react'
 export default class Saudacao extends Component {
 
     state = {
-
         tipo: this.props.tipo,
         nome: this.props.nome
     }
 
+    constructor(props) {
+        super(props)
+
+        this.setTipo = this.setTipo.bind(this)
+        //this.setNome = this.setNome.bind(this)
+    }
+
     setTipo(e) {        
         this.setState({ tipo: e.target.value })
-    }
+    }   
 
     setNome(e) {        
         this.setState({ nome: e.target.value })
@@ -24,10 +30,10 @@ export default class Saudacao extends Component {
                 <h1>{tipo} {nome}!</h1>
                 <hr />
                 <input type="text" placeholder="Tipo..."
-                    value={tipo} onChange={e => this.setTipo(e)}/>
+                    value={tipo} onChange={this.setTipo}/>
 
                 <input type="text" placeholder="Nome..." 
-                    value={nome} onChange={e => this.setNome(e)}/>
+                    value={nome} onChange={e =>this.setNome(e)}/>
             </div>
         )
     }
