b92ab0e91969e26df7652dc20bef658473345eae
Eduardo.silva <eduardo.jose184@gmail.com>
2021-09-08 13:29:18 -0300

    react - user registration project - adding footer component

--- react/crud/frontend/src/components/template/Footer.jsx
+++ react/crud/frontend/src/components/template/Footer.jsx
@@ -3,5 +3,8 @@ import React from 'react'
 
 export default props =>
     <footer className="footer">
-        Rodapé
+        <span>
+            Desenvolvido com <i className="fa fa-heart text-danger"></i> por
+            <strong> Eduar<span className="text-danger">d</span>o</strong>
+        </span>
     </footer>
