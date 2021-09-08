2b7ce0789a992c6ac68fb25958da99b2c767b764
eduardo7321 <eduardo.jose184@gmail.com>
2021-09-08 20:51:30 -0300

    react - user registration project - adding main v2 component

--- react/crud/frontend/src/main/App.jsx
+++ react/crud/frontend/src/main/App.jsx
@@ -5,18 +5,13 @@ import React from 'react'
 
 import Logo from '../components/template/Logo'
 import Nav from '../components/template/Nav'
-import Main from '../components/template/Main'
+import Home from '../components/home/Home'
 import Footer from '../components/template/Footer'
 
 export default props =>
     <div className="app">
         <Logo />
         <Nav />
-        <Main icon="home" title="Início"
-            subtitle="Segundo Projeto do capítulo de React">
-            <div className="display-4">Bem Vindo!</div>
-            <p className="mb-0">Sistema para exemplificar a construção de um cadastro desenvolvido em React!</p>
-
-            </Main>
+        <Home />
         <Footer />
     </div>
