e14b1e2f0928d5bae5a5d120c08a4d0ceb8d9bc5
eduardo7321 <eduardo.jose184@gmail.com>
2021-10-16 18:24:24 -0300

    vueJS - projeto calculadora - implementando a logia

--- vue/calculator/src/main/Calculator.vue
+++ vue/calculator/src/main/Calculator.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="calculator">
-      <Display value="10000" />
+      <Display :value="displayValue" />
       <Button label="AC" triple @meuOnClick="clearMemory" />      
       <Button label="/" operation @meuOnClick="setOperation" />      
       <Button label="7" @meuOnClick="addDigit" />      
@@ -27,16 +27,67 @@ import Button from "../components/Button"
 import Display from "../components/Display"
 
 export default {
+    data: function() {
+        return {
+            displayValue: "0",
+            clearDisplay: false,
+            operation: null,
+            values: [0, 0],
+            current: 0
+        }
+    },
     components: { Button, Display},
     methods: {
         clearMemory() {
-            console.log('Limpar Memória!')
+            Object.assign(this.$data, this.$options.data())
         },
         setOperation(operation) {
-            console.log('Operação ' + operation)
+            if (this.current === 0) {
+                this.operation = operation
+                this.current = 1
+                this.clearDisplay = true
+            } else {
+                const equals = operation === "="
+                const currentOperation = this.operation
+               
+               try {
+                   this.values[0] = eval(
+                       `${this.values[0]} ${currentOperation} ${this.values[1]}`
+                   )
+               } catch (e) {
+                   this.$emit('onError', e)
+               }
+
+               this.values[1] = 0
+
+               this.displayValue = this.values[0]
+               this.operation = equals ? null : operation
+               this.current = equals ? 0 : 1
+               this.clearDisplay = !equals
+            }
         },
         addDigit(n) {
-            console.log('Digito ' + n)
+            if (n === "." && this.displayValue.includes(".")) {
+                return
+            }
+
+            const clearDisplay = this.displayValue === "0"
+                || this.clearDisplay
+            const currentValue = clearDisplay ? "" : this.displayValue
+            const displayValue = currentValue + n
+
+            this.displayValue = displayValue
+            this.clearDisplay = false
+
+            //alternativa 1
+            this.values[this.current] = displayValue
+
+            //alternativa 2
+          //  if (n !==".") {
+        //        const i = this.current
+           //     const newValue = parseFloat(displayValue)
+            //    this.values[i] = newValue
+            //}
         }
     }
 
