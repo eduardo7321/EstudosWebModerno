5c6c639b8989eaad2dd337b2d05f1a8fccec7332
eduardo7321 <eduardo.jose184@gmail.com>
2021-10-16 17:08:59 -0300

    vueJS - projeto calculadora - criando os componentes Btton e Display

---
+++ vue/calculator/src/main/Calculator.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="calculator">
+      <Display value="10000" />
+      <Button label="AC" triple @meuOnClick="clearMemory" />      
+      <Button label="/" operation @meuOnClick="setOperation" />      
+      <Button label="7" @meuOnClick="addDigit" />      
+      <Button label="8" @meuOnClick="addDigit" />      
+      <Button label="9" @meuOnClick="addDigit" />      
+      <Button label="*" operation @meuOnClick="setOperation" />      
+      <Button label="4" @meuOnClick="addDigit" />      
+      <Button label="5" @meuOnClick="addDigit" />      
+      <Button label="6" @meuOnClick="addDigit" />      
+      <Button label="-" operation @meuOnClick="setOperation" />      
+      <Button label="1" @meuOnClick="addDigit" />      
+      <Button label="2" @meuOnClick="addDigit" />      
+      <Button label="3" @meuOnClick="addDigit" />      
+      <Button label="+" operation @meuOnClick="setOperation" />      
+      <Button label="0" double @meuOnClick="addDigit" />      
+      <Button label="." @meuOnClick="addDigit" />      
+      <Button label="=" operation @meuOnClick="setOperation" />    
+         
+    </div>
+</template>
+
+<script>
+import Button from "../components/Button"
+import Display from "../components/Display"
+
+export default {
+    components: { Button, Display},
+    methods: {
+        clearMemory() {
+            console.log('Limpar Memória!')
+        },
+        setOperation(operation) {
+            console.log('Operação ' + operation)
+        },
+        addDigit(n) {
+            console.log('Digito ' + n)
+        }
+    }
+
+}
+</script>
+
+<style>
+.calculator {
+    height: 321px;
+    width: 235px;
+    border-radius: 5px;
+    overflow: hidden;
+
+    display: grid;
+    grid-template-columns: repeat(4, 25%) ;
+    grid-template-rows: 1fr 48px 48px 48px 48px 48px;
+}
+</style>
